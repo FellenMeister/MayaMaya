84d750fb9a2c25b556a1406948ab7209c494438a
FellenMeister <leslie_fellensiek@hotmail.com>
2015-12-21 14:07:07 +0100

    rename

--- MayaMaya/Inlogscherm.Designer.cs
+++ MayaMaya/Inlogscherm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MayaMaya
 {
-    partial class Form1
+    partial class Inlogscherm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inlogscherm));
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
