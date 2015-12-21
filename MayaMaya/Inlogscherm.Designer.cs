6a87d4bfaf452d54eb367e1492b6b47458483539
FellenMeister <leslie_fellensiek@hotmail.com>
2015-12-21 14:30:15 +0100

    ;

--- MayaMaya/Inlogscherm.Designer.cs
+++ MayaMaya/Inlogscherm.Designer.cs
@@ -29,56 +29,54 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inlogscherm));
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Txt_Ww = new System.Windows.Forms.TextBox();
+            this.Txt_Login = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.Btn_Login = new System.Windows.Forms.Button();
+            this.Btn_Help = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox2
+            // Txt_Ww
             // 
-            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
-            this.textBox2.Location = new System.Drawing.Point(287, 466);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(198, 20);
-            this.textBox2.TabIndex = 23;
-            this.textBox2.Text = "Wachtwoord";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox2.UseWaitCursor = true;
+            this.Txt_Ww.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Ww.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
+            this.Txt_Ww.Location = new System.Drawing.Point(281, 434);
+            this.Txt_Ww.Name = "Txt_Ww";
+            this.Txt_Ww.Size = new System.Drawing.Size(198, 26);
+            this.Txt_Ww.TabIndex = 23;
+            this.Txt_Ww.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox1
+            // Txt_Login
             // 
-            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
-            this.textBox1.Location = new System.Drawing.Point(287, 411);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(198, 20);
-            this.textBox1.TabIndex = 22;
-            this.textBox1.Text = "Inlognaam";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox1.UseWaitCursor = true;
+            this.Txt_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
+            this.Txt_Login.Location = new System.Drawing.Point(281, 379);
+            this.Txt_Login.Name = "Txt_Login";
+            this.Txt_Login.Size = new System.Drawing.Size(198, 26);
+            this.Txt_Login.TabIndex = 22;
+            this.Txt_Login.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button3
             // 
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(14, 45);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(124, 59);
             this.button3.TabIndex = 21;
             this.button3.Text = "Login";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.UseWaitCursor = true;
             // 
             // label5
             // 
@@ -86,12 +84,11 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
-            this.label5.Location = new System.Drawing.Point(311, 434);
+            this.label5.Location = new System.Drawing.Point(305, 402);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(157, 29);
             this.label5.TabIndex = 20;
             this.label5.Text = "Wachtwoord";
-            this.label5.UseWaitCursor = true;
             // 
             // label4
             // 
@@ -99,19 +96,17 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
-            this.label4.Location = new System.Drawing.Point(346, 379);
+            this.label4.Location = new System.Drawing.Point(340, 347);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 29);
             this.label4.TabIndex = 19;
             this.label4.Text = "Login";
-            this.label4.UseWaitCursor = true;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
-            this.pictureBox2.Enabled = false;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(648, 903);
             this.pictureBox2.Name = "pictureBox2";
@@ -119,8 +114,6 @@
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 18;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.UseWaitCursor = true;
-            this.pictureBox2.Visible = false;
             // 
             // label3
             // 
@@ -133,7 +126,6 @@
             this.label3.Size = new System.Drawing.Size(93, 20);
             this.label3.TabIndex = 17;
             this.label3.Text = "Powered By";
-            this.label3.UseWaitCursor = true;
             // 
             // label2
             // 
@@ -146,27 +138,12 @@
             this.label2.Size = new System.Drawing.Size(193, 20);
             this.label2.TabIndex = 16;
             this.label2.Text = "Bestelsysteem MayaMaya";
-            this.label2.UseWaitCursor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(226)))));
-            this.label1.Location = new System.Drawing.Point(690, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 55);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "?";
-            this.label1.UseWaitCursor = true;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
-            this.pictureBox1.Enabled = false;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(14, 895);
             this.pictureBox1.Name = "pictureBox1";
@@ -174,61 +151,92 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.UseWaitCursor = true;
-            this.pictureBox1.Visible = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
+            this.button2.Enabled = false;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Location = new System.Drawing.Point(-1, 884);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(1356, 106);
+            this.button2.Size = new System.Drawing.Size(754, 106);
             this.button2.TabIndex = 13;
+            this.button2.TabStop = false;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseMnemonic = false;
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.UseWaitCursor = true;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.button1.Enabled = false;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(-1, -2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(1356, 106);
+            this.button1.Size = new System.Drawing.Size(754, 106);
             this.button1.TabIndex = 12;
             this.button1.TabStop = false;
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button1.UseMnemonic = false;
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.UseWaitCursor = true;
             // 
-            // Form1
+            // Btn_Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Btn_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
+            this.Btn_Login.FlatAppearance.BorderSize = 0;
+            this.Btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(226)))));
+            this.Btn_Login.Location = new System.Drawing.Point(328, 470);
+            this.Btn_Login.Name = "Btn_Login";
+            this.Btn_Login.Size = new System.Drawing.Size(100, 40);
+            this.Btn_Login.TabIndex = 24;
+            this.Btn_Login.Text = "Inloggen";
+            this.Btn_Login.UseVisualStyleBackColor = false;
+            // 
+            // Btn_Help
+            // 
+            this.Btn_Help.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
+            this.Btn_Help.FlatAppearance.BorderSize = 0;
+            this.Btn_Help.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Help.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Help.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(226)))));
+            this.Btn_Help.Location = new System.Drawing.Point(692, 19);
+            this.Btn_Help.Name = "Btn_Help";
+            this.Btn_Help.Size = new System.Drawing.Size(48, 67);
+            this.Btn_Help.TabIndex = 25;
+            this.Btn_Help.Text = "?";
+            this.Btn_Help.UseVisualStyleBackColor = false;
+            // 
+            // Inlogscherm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(226)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(752, 985);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Btn_Help);
+            this.Controls.Add(this.Btn_Login);
+            this.Controls.Add(this.Txt_Ww);
+            this.Controls.Add(this.Txt_Login);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(74)))), ((int)(((byte)(54)))));
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Inlogscherm";
+            this.Text = "MayaMaya";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -238,18 +246,19 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Txt_Ww;
+        private System.Windows.Forms.TextBox Txt_Login;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_Login;
+        private System.Windows.Forms.Button Btn_Help;
     }
 }
 
